a16b75210eb71feb15dca398000ab5f6d2e2b4c5
David Dohnal <dohnal@gjs.cz>
2022-11-18 15:14:52 +0100

    Update server.js
    Create players array with params

--- server.js
+++ server.js
@@ -4,17 +4,17 @@ const http = require('http');
 const path = require('path');
 const socketIO = require('socket.io')
 
+const { historyQ, historyA, geographyQ, geographyA, mathQ, mathA } = require('./questionExtract');
 
+
+console.log(historyQ);
 const app = express()
 var server = http.Server(app)
 var io = socketIO(server, {
     pingTimeout: 60000,
 })
 
-let rawQuestions = fs.readFileSync('questions.json');
-let questions = JSON.parse(rawQuestions);
 
-console.log(questions.history[0].q);
 var players = {}
 var questionToAsk;
 
@@ -29,23 +29,56 @@ app.get('/', function (request, response) {
 io.on('connection', function (socket) {
     console.log('player [' + socket.id + '] connected')
 
+    players[socket.id] = {
+        //first level params
+        completeFirstLevel: false,
+        questionsFirstLevel: historyQ,
+        answersFirtLevel: historyA,
+        randomQuestionsFirstLevel: Array.from(Array(historyQ.length).keys()).sort((a, b) => 0.5 - Math.random()), //generate sequence of questions
+        chestFirstLevelID: [1, 2, 3, 4],
+
+        //second level params
+        completeSecondLevel: false,
+        questionsSecondLevel: geographyQ,
+        answersSecondLevel: geographyA,
+        randomQuestionsSecondLevel: Array.from(Array(geographyQ.length).keys()).sort((a, b) => 0.5 - Math.random()),
+        chestSecondLevelID: [1, 2, 3, 4],
+
+        //third level params
+        completeThirdLevel: false,
+        questionsThirdLevel: mathQ,
+        answersThirdLevel: mathA,
+        randomQuestionsThirdLevel: Array.from(Array(mathQ.length).keys()).sort((a, b) => 0.5 - Math.random()),
+        chestThirdLevelID: [1, 2, 3, 4],
+
+    }
     socket.on('getQuestion', (arg) => {
         console.log('player: ' + socket.id + ' -> message: ' + arg);
-        if (arg == 1) {
-            questionToAsk = questions.history[0].q
+
+        //check if chest.id is equal of chestFirstLevel[0]
+        if (arg == players[socket.id].chestFirstLevelID[0]) {
+            questionToAsk = players[socket.id].questionsFirstLevel[players[socket.id].randomQuestionsFirstLevel[0]];
             socket.emit('questionToAsk', questionToAsk);
             console.log('Message: ' + questionToAsk + ' sent to player with id:' + socket.id);
+
+            console.log('waiting for answer');
+            socket.on('answer', (arg) => {
+                console.log('player answer is: ' + arg);
+                if (players[socket.id].answersFirtLevel[players[socket.id].randomQuestionsFirstLevel[0]] == arg) {
+                    socket.emit('result', true);
+
+                    players[socket.id].randomQuestionsFirstLevel.shift();
+                    console.log('Rest of random question: ' + players[socket.id].randomQuestionsFirstLevel);
+
+                    players[socket.id].chestFirstLevelID.shift();
+                    console.log('Rest of chestID to pick up: ' + players[socket.id].chestFirstLevelID);
+                }
+                else {
+                    socket.emit('result', false);
+                }
+            })
         }
-        console.log('waiting for answer');
-        socket.on('answer', (arg) => {
-            console.log('player answer is: ' + arg);
-            if (questions.history[0].a == arg) {
-                socket.emit('result', true);
-            }
-            else {
-                socket.emit('result', false);
-            }
-        })
+
     })
 })
 
